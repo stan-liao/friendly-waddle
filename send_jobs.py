0aaf85d6791f7196c26265c768d4a844f46e0a7d
stan-liao <stan.liao@gmail.com>
2023-05-14 21:32:08 -0400

    refactoring code

--- send_jobs.py
+++ send_jobs.py
@@ -10,11 +10,10 @@ Created on Sun Mar 19 21:43:47 2023
 import os
 import win32ui
 import win32com.client
-import time
 from bs4 import BeautifulSoup
 import json
-
-print(os.getcwd())
+import argparse
+import pandas as pd
 
 #prints filename to destination uni
 def print_file(filename, uni):
@@ -50,58 +49,83 @@ def is_uni(uni):
 
 #turn name from last, first middle to first middle last
 def rework_name(name):
+    #strip whitespace from end of name
+    name = name.strip()
     name = name.split(", ")
     name = name[1] + " " + name[0]
     return name
 
-def print_all(json_file, print_files):
+#reads a json from a directory, parses it into a dataframe
+def read_dir_json(json_file):
+    df = pd.DataFrame(columns=["name", "email", "uni", "title", "department"])
+    my_list = []
+    for entry in json_file:
+        dict = {}
+
+        #check if valid email address
+        try:
+            dict["uni"] = entry["email"].split("@")[0]
+        except:
+            #invalid email entry
+            continue
+        role = entry["role"]
+        dict["title"], dict["dpt"] = get_roles(role)
+        name = rework_name(entry["name"])   
+        dict["name"] = entry["name"]
+        my_list.append(dict)
+    
+    #list of dicts to dataframe
+    df = pd.DataFrame.from_dict(my_list)
+    return df
+
+def print_all(json_file, print_files, start_name):
     #iterate through all entries in json file
     i = 0
-
-    for entry in json_file:
-        #print each file
+    if(start_name != ""):
+        seen = False
+    df = read_dir_json(json_file)
+    #iterate through dataframe
+    for entry in df.itertuples():
         to_print = print_files[i]
         i = (i+1)%len(print_files)
         
-        #check if string is in form letter letter number number number number
-        try:
-            uni = entry["email"].split("@")[0]
-        except:
-            #no email entry
-            continue
-
-        role = entry["role"]
-        title, dpt = get_roles(role)
-        name = rework_name(entry["name"])        
-
-        if is_uni(uni) and (title == "Student, COLUMBIA COLLEGE" or
-                            title == "Student, FU FOUNDATN SCHL OF ENGINEERING & APPLIED SCIENCE:UGRAD"):
-            print("found one that works?")
-            text =  "Hello " + name + "\n\n"
-            text += "Good luck on your " + dpt + " finals!\n\n"
-            text += to_print
-
-            #write text string to file
+        if is_uni(entry.uni) and (entry.title == "Student, COLUMBIA COLLEGE" or
+                                    entry.title == "Student, FU FOUNDATN SCHL OF ENGINEERING & APPLIED SCIENCE:UGRAD"):
+            text = "Hi " + entry.name + "\n\n"
+            text += "Good luck on your " + entry.department + " finals!\n\n"
+            text += "----------------------------------------\n\n"
+            text += to_print 
             with open("temp.txt", "w") as f:
                 f.write(text)
-            print_file("temp.txt", uni)
-            return
-    
+            if seen:
+                print_file("temp.txt", entry["uni"])
+            if entry.name == start_name:
+                seen = True
+        
+    return
+   
 
 #main function here
 if __name__ == "__main__":
-    filenames = ["job1.txt", "job2.txt"]
+    #take in args from command line
+    parser = argparse.ArgumentParser(description='Send jobs to printer.')
+    parser.add_argument('--startname', "-name", type=str, default="", help='if restarting program, enter name of last person printed')
+    parser.add_argument("--files", "-f", type=str, nargs="+", default=["job1.txt", "job2.txt"], help="list of files to print")
+    parser.add_argument("--datafile", type=str, default="scraper/data2.json", help="json file to read from")
+
+    args = parser.parse_args()
 
     print_files = []
 
     #read filenames into strings
-    for file in filenames:
+    for file in args.files:
         with open(file, "r") as f:
             text = f.read()
             print_files.append(text)
 
     #read json into dictionary
-    with open("scraper/data2.json", "r") as f:
+    with open(args.datafile, "r") as f:
         json_file = json.load(f)
 
-    print_all(json_file, print_files)
+    print_all(json_file, print_files, args.startname)
+
